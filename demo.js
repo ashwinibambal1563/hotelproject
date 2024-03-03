d2173e5b6470171c7ea9d72ae7c1888c3388a610
ashwinibambal1563 <ashwinitayade22@gmail.com>
2024-03-03 12:30:11 +0530

    done

---
+++ demo.js
@@ -0,0 +1,12 @@
+num1=100
+num2=200
+console.log("welcome to js")
+console.log("addition is=",num1+num2)
+console.log("mul=",num1*num2)
+console.log("sub=",num1-num2)
+console.log("div=",num1/num2)
+name="hii i am ashwini bambal" 
+console.log(name,"doing full stack development course")
+console.log("use square and cube")
+console.log("square of 100=",num1*num1)
+console.log("cube of 200=",num1*num1*num1)
